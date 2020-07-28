7e3b915a1181fdf5d29aa05bc7e40524b162ffdb
Chloe <chloe@localhost.localdomain>
2020-07-28 02:32:38 -0400

    Commit 3

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '90991386503f7cbfad768b9173025cdd' #yea i know this isnt safe but its a school project
+# so i will leave it like this for now and use github secrets later :)
 
 @app.route('/')
 @app.route('/home')
 def home_route():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def registration_route():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home_route'))
     return render_template('register.html', form=form)
 
 @app.route('/login')
--- forms.py
+++ forms.py
@@ -3,10 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15, message=None)])
-    email = StringField('Email', validators=[DataRequired(), Length(min=15, max=40), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm = PasswordField('Confirm Password', validators=[EqualTo('confirm', message="Password doesn't match!")])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('confirm', message="Password doesn't match!")])
     submit = SubmitField('Register')
 
 class LoginForm():
