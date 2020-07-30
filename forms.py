c0ef0237946f419a1c4f5b23a00fbe7ceb0436e8
itsOmniscient <omniscient1337@outlook.com>
2020-07-30 23:58:49 +0200

    Font changes

--- forms.py
+++ forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=25)])
-    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Password doesn't match!")])
-    submit = SubmitField('Register')
+    username = StringField('Корисничко име', validators=[DataRequired(), Length(min=4, max=15)])
+    email = StringField('E-mail адреса', validators=[DataRequired(), Email()])
+    password = PasswordField('Лозинка', validators=[DataRequired(), Length(min=5, max=25)])
+    confirm_pass = PasswordField('Внесете ја лозинката повторно', validators=[DataRequired(), EqualTo('password', message="Password doesn't match!")])
+    submit = SubmitField('Регистрирај се')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    button = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    email = StringField('E-mail адреса', validators=[DataRequired(), Email()])
+    password = PasswordField('Лозинка', validators=[DataRequired()])
+    button = BooleanField('Запамти ме')
+    submit = SubmitField('Најава')
