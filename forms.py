65c4f548348a6c541b3e42772572e9fb922aebde
itsOmniscient <omniscient1337@outlook.com>
2020-07-30 17:40:40 +0200

    More logic for registration page

--- forms.py
+++ forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('confirm', message="Password doesn't match!")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=25)])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Password doesn't match!")])
     submit = SubmitField('Register')
 
 class LoginForm():
