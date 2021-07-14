3e81162f3b154bc14c67490aef3ebb71f9d8f7d3
itsOmniscient <omniscient1337@outlook.com>
2021-07-14 23:42:30 +0200

    Disabled Flask-SQLAlchemy event notification system

--- movies_webstore/__init__.py
+++ movies_webstore/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("CSRF_SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yicdarshqphnbc:5d55d883b19400882160d1f9603b6d1435d8fcb098eab8def93691df99905f4f@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/dfm9lrak34ac2o'
 db = SQLAlchemy(app)
